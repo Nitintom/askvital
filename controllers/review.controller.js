c472d7a7aa57173b636c9a6359be183a84ea5ace
Nitin Tomar <114360708+Nitintom@users.noreply.github.com>
2023-11-22 13:28:10 +0530

    Update review.controller.js

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -1,7 +1,7 @@
-import Review from "../models/healthcare.model.js"; // Import the Review model
-import Healthcare from "../models/healthcare.model.js"; // Import the Healthcare model
+import mongoose from "mongoose";
+import Healthcare from "../models/healthcare.model.js";
 
-// Create a new review
+// Create a new review associated with a healthcare entity
 export const createReview = async (req, res) => {
   try {
     const {
@@ -11,29 +11,29 @@ export const createReview = async (req, res) => {
       efficiencyRating,
       costRange,
       review,
-      user,
+      userName, // Add userName
     } = req.body;
 
     // Get the healthcare entity ID from the request parameters
-    const { id } = req.params; // Correct variable name
+    const { id } = req.params;
 
     // Find the healthcare entity by its ID
-    const healthcareEntity = await Healthcare.findById(id); // Correct variable name
+    const healthcareEntity = await Healthcare.findById(id);
 
     if (!healthcareEntity) {
       return res.status(404).json({ error: "Healthcare entity not found." });
     }
 
-    // Create a new review associated with the healthcare entity
-    const newReview = new Review({
+    // Create a new review object
+    const newReview = {
       qualityRating,
       compassionRating,
       cleanlinessRating,
       efficiencyRating,
       costRange,
       review,
-      user,
-    });
+      userName, // Add userName
+    };
 
     // Push the new review to the healthcare entity's reviews array
     healthcareEntity.reviews.push(newReview);
@@ -46,14 +46,21 @@ export const createReview = async (req, res) => {
     console.error("Error creating review:", error);
     res
       .status(500)
-      .json({ error: "An error occurred while creating the review", error });
+      .json({ error: "An error occurred while creating the review" });
   }
 };
 
-// Get all reviews
+// Get all reviews of a healthcare entity
 export const getAllReviews = async (req, res) => {
   try {
-    const reviews = await Review.find();
+    const { id } = req.params; // Get the healthcare entity ID from the request parameters
+    const healthcareEntity = await Healthcare.findById(id);
+
+    if (!healthcareEntity) {
+      return res.status(404).json({ error: "Healthcare entity not found." });
+    }
+
+    const reviews = healthcareEntity.reviews;
     res.status(200).json(reviews);
   } catch (error) {
     res
@@ -65,8 +72,14 @@ export const getAllReviews = async (req, res) => {
 // Get a single review by ID
 export const getReviewById = async (req, res) => {
   try {
-    const { id } = req.params;
-    const review = await Review.findById(id);
+    const { id, reviewId } = req.params; // Get the healthcare entity ID and review ID from the request parameters
+    const healthcareEntity = await Healthcare.findById(id);
+
+    if (!healthcareEntity) {
+      return res.status(404).json({ error: "Healthcare entity not found." });
+    }
+
+    const review = healthcareEntity.reviews.id(reviewId);
     if (!review) {
       return res.status(404).json({ error: "Review not found." });
     }
@@ -79,35 +92,83 @@ export const getReviewById = async (req, res) => {
 };
 
 // Update a review by ID
-export const updateReviewById = async (req, res) => {
+export const updateReviewByNameAndId = async (req, res) => {
   try {
-    const { id } = req.params;
-    const updatedReview = await Review.findByIdAndUpdate(id, req.body, {
-      new: true,
+    const { name: healthcareEntityName, reviewId } = req.params;
+    const updatedFields = req.body;
+
+    // Check if reviewId is a valid ObjectId before querying the database
+    if (!mongoose.Types.ObjectId.isValid(reviewId)) {
+      return res.status(400).json({ error: "Invalid reviewId." });
+    }
+
+    const healthcareEntity = await Healthcare.findOne({
+      name: healthcareEntityName,
     });
-    if (!updatedReview) {
+
+    if (!healthcareEntity) {
+      return res.status(404).json({ error: "Healthcare entity not found." });
+    }
+
+    const review = healthcareEntity.reviews.id(reviewId);
+
+    if (!review) {
       return res.status(404).json({ error: "Review not found." });
     }
-    res.status(200).json(updatedReview);
+
+    // Update the review object
+    review.set(updatedFields);
+
+    const updatedHealthcareEntity = await healthcareEntity.save();
+
+    // Send only the relevant data back to the client
+    const { _id, name, reviews } = updatedHealthcareEntity;
+    const responseData = { _id, name, reviews };
+
+    res.status(200).json(responseData);
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while updating the review." });
+    console.error("Error updating review:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-// Delete a review by ID
-export const deleteReviewById = async (req, res) => {
+export const deleteReviewByNameAndId = async (req, res) => {
   try {
-    const { id } = req.params;
-    const deletedReview = await Review.findByIdAndRemove(id);
-    if (!deletedReview) {
+    const { name: healthcareEntityName, reviewId } = req.params;
+
+    // Check if reviewId is a valid ObjectId before querying the database
+    if (!mongoose.Types.ObjectId.isValid(reviewId)) {
+      return res.status(400).json({ error: "Invalid reviewId." });
+    }
+
+    const healthcareEntity = await Healthcare.findOne({
+      name: healthcareEntityName,
+    });
+
+    if (!healthcareEntity) {
+      return res.status(404).json({ error: "Healthcare entity not found." });
+    }
+
+    const reviewIndex = healthcareEntity.reviews.findIndex(
+      (review) => review._id.toString() === reviewId
+    );
+
+    if (reviewIndex === -1) {
       return res.status(404).json({ error: "Review not found." });
     }
-    res.status(200).json(deletedReview);
+
+    // Remove the review from the array
+    healthcareEntity.reviews.splice(reviewIndex, 1);
+
+    const updatedHealthcareEntity = await healthcareEntity.save();
+
+    // Send only the relevant data back to the client
+    const { _id, name, reviews } = updatedHealthcareEntity;
+    const responseData = { _id, name, reviews };
+
+    res.status(200).json(responseData);
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while deleting the review." });
+    console.error("Error deleting review:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
