a895ba66692c4e9ddd826f98880d58eac76d6105
Nitin Tomar <114360708+Nitintom@users.noreply.github.com>
2023-11-22 13:21:02 +0530

    Update user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,6 +5,7 @@ import {
   adminRoute,
   signup, // Import the signup function
   signin, // Import the signin function
+  deleteUserByAdmin,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/verifyUser.js";
 import { isAdmin } from "../middleware/isAdmin.js";
@@ -13,10 +14,13 @@ const router = express.Router();
 
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-router.post("/signup", signup); // Route for user registration
+router.post("/signup", signup); // Route for ser registration
 router.post("/signin", signin); // Route for user login
 
 // Admin-only route
-router.get("/admin-route", isAdmin, adminRoute);
+router.get("/admin-route", adminRoute);
+
+// Admin-only route to delete a user
+router.delete("/admin-delete/:id", deleteUserByAdmin);
 
 export default router;
