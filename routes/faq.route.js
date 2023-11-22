421fc28c47ec2450804e4fa7150b15e2298ca220
Nitin Tomar <114360708+Nitintom@users.noreply.github.com>
2023-11-22 13:22:06 +0530

    Update faq.route.js

--- routes/faq.route.js
+++ routes/faq.route.js
@@ -2,18 +2,22 @@ import express from "express";
 import {
   createFAQ,
   updateFAQ,
+  deleteFAQ,
   getAllFAQs,
 } from "../controllers/faq.controller.js";
 
 const router = express.Router();
 
 // Create a new FAQ for a Healthcare Entity
-router.post("/api/healthcare/:id/faq", createFAQ);
+router.post("/healthcare/:id/faq", createFAQ);
 
 // Update an FAQ for a Healthcare Entity
-router.put("/api/healthcare/:id/faq/:faqId", updateFAQ);
+router.put("/healthcare/:name/faq/:faqId", updateFAQ);
+
+// Delete an FAQ for a Healthcare Entity
+router.delete("/healthcare/:name/faq/:faqId", deleteFAQ);
 
 // Get all FAQs for a Healthcare Entity
-router.get("/api/healthcare/:id/faqs", getAllFAQs);
+router.get("/healthcare/:id/faqs", getAllFAQs);
 
 export default router;
