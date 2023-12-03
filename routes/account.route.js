e713dcc4bcab0981b08d28301439a5b5e8ac98fc
Nitin Tomar <114360708+Nitintom@users.noreply.github.com>
2023-12-03 15:18:38 +0530

    Update account.route.js

--- routes/account.route.js
+++ routes/account.route.js
@@ -1,1 +1,20 @@
+import express from "express";
+import * as accountController from "../controllers/account.controller.js";
+
+const router = express.Router();
+
+router.post("/account/signup", accountController.signup);
+router.post("/account/signin", accountController.signin);
+router.post("/account/google", accountController.google);
+router.post("/account/signout", accountController.signout);
+
+// Additional routes
+router.put("/account/update/:id", accountController.updateUser);
+router.delete("/account/delete/:id", accountController.deleteUser);
+
+// Admin routes
+router.get("/account/admin/users", accountController.adminRoute);
+router.delete("/account/admin/delete/:id", accountController.deleteUserByAdmin);
+
+export default router;
 
